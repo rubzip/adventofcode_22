c7f2a2b1cff7819af7bf6e8ad1a5afc18548d104
Rubén <74114026+rubzip@users.noreply.github.com>
2022-12-09 16:04:37 +0100

    Add files via upload

--- generate_dir.sh
+++ generate_dir.sh
@@ -1,9 +1,7 @@
 day=$(date +"%d")
 
-# Crear la carpeta "day_08"
 mkdir "day_$day"
 
-# Crear el archivo "08.py" dentro de la carpeta "day_08" y agregar contenido
-touch "day_$day/08.py"
-echo "with open('input') as f:" >> "day_$day/08.py"
-echo "\tdata = f.read()" >> "day_$day/08.py"
+touch "day_$day/$day.py"
+echo "with open('input') as f:" >> "day_$day/$day.py"
+echo "\tdata = f.read()" >> "day_$day/$day.py"
