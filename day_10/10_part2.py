d5b8756024541735396a82a440fd4d435e5fa8ba
Rubén <74114026+rubzip@users.noreply.github.com>
2022-12-10 20:22:14 +0100

    Add files via upload

--- day_10/10_part2.py
+++ day_10/10_part2.py
@@ -1,7 +1,14 @@
+def sprite(x):
+    if x<39:
+        return list(range(x, x+3))
+    else:
+        return list(map(lambda y:(y-1)%40+1, range(x, x+3)))
+
+
 def new_pixel(x, cycle):
-    sprite = range(x, x+3)
-    new_line = '' if cycle%40 else '\n'
-    if cycle in sprite:
+    aux_cycle = (cycle-1)%40+1
+    new_line = '' if aux_cycle!=40 else '\n'
+    if aux_cycle in sprite(x):
         return '#'+new_line
     else:
         return '.'+new_line
@@ -20,7 +27,7 @@ CRT = ''
 for command in commands:
     n_of_cycles = 2 if command[0]=='addx' else 1
     for i in range(n_of_cycles):
-        CRT += new_pixel(x, cycle%40)
+        CRT += new_pixel(x, cycle)
         cycle += 1
     if n_of_cycles==2:
         x += int(command[1])
